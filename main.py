ea796f1c5da8f2857dadd481547ecc7f99c7cffa
Jesse Carvalho Amorim <jessecaramorim@gmail.com>
2022-09-10 12:02:53 -0300

    Tamanho da Pizza!

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import Tamanho
+traço = 5*"-"
 print('--->Bem Vindo a Pizzaria RPM<---')
-print('--------------------------------')
+print(traço)
 print('-->Este é o nosso Cardápio! Deseja qual sabor?<--')
 saborP1 = 15
 saborP2 = 15
@@ -7,12 +9,15 @@ saborP3 = 10
 saborP4 = 5
 saborP5 = 5
 saborP0 = 'Nenhum, desisti voltarei outra hora!'
-# Sabor da Pizza
-cont = 2
-while cont == 2:
-      saborPizza = int(input('1 - Pepperoni (+ R$ 15,00)\n '
+# Tamanho da Pizza (valor)
+tamP1 = 15
+tamP2 = 20
+tamP3 = 25
+tamP4 = 30
+
+saborPizza = int(input('1 - Pepperoni (+ R$ 15,00)\n '
       '(Molho de Tomate - Mussarela - Pepperoni)\n'
-      '\n'      
+      '\n'
       '2 - Quatro Queijos (+ 15,00)\n(Molho de Tomate - Mussarela - Parmesão - Provolone - Gorgonzola)\n'
       '\n'
       '3 - Frango e Catupiry (+ R$ 10,00)\n(Molho de Tomate - Mussarela - Frango Desfiado - Catupiry - Cebola)\n'
@@ -20,43 +25,62 @@ while cont == 2:
       '4 - Calabresa (+ R$ 5,00)\n(Molho de Tomate - Mussarela - Calabresa - Cebola)\n'
       '\n'
       '5 - Marguerita (+ R$ 5,00)\n(Molho de Tomate - Mussarela - Tomate - Mangericão)\n'
-                       '\n'
-                       '0 - Nenhum, desisti voltarei outra hora!\n'
-                       '\n'
- 
-                       'Digite aqui o número: '))
-      qtde_sabor = int(input('Prefere somente um sabor? Pode escolher até 2!\n'
-                       'Digite aqui --> '))
+      '\n'
+      '0 - Nenhum, desisti voltarei outra hora!\n'
+      '\n'
+      'Digite aqui o número: '))
 
-      if saborPizza == 0:
-            print(f'--------------------------------\n'
-                  f'Valor atual: R$ 0,0 {saborP0}\n'
-                  f'--------------------------------')
-      if saborPizza == 1:
-            if qtde_sabor == 1:
-                  print('Ok! Voce escolheu apenas 1 sabor!')
-                  print(f'--------------------------------\n'
-                  f'Valor atual está: R$ {saborP1}\n'
-                  f'--------------------------------')
-            elif qtde_sabor == 2:
-                  print('Ok! Voce escolheu 2 sabores!')
-                  print(f'--------------------------------\n'
-                        f'Valor atual está: R$ {saborP1 + 1}\n'
-                        f'--------------------------------')
-
-      elif saborPizza == 2:
-            print(f'--------------------------------\n'
-                  f'Valor atual: R$ {saborP2}\n'
-                  f'--------------------------------')
-      elif saborPizza == 3:
-            print(f'--------------------------------\n'
-                  f'Valor atual: R$ {saborP3}\n'
-                  f'--------------------------------')
-      elif saborPizza == 4:
-            print(f'--------------------------------\n'
-                  f'Valor atual: R$ {saborP4}\n'
-                  f'--------------------------------')
-      elif saborPizza == 5:
-            print(f'--------------------------------\n'
-                  f'Valor atual: R$ {saborP5}\n'
-                  f'--------------------------------')
+# Sabor da Pizza
+if saborPizza == 0:
+      print(traço)
+      print('Ok, até logo!!!')
+      print(traço)
+elif saborPizza == 1:
+      print(traço)
+      print(f'Valor atual está: R$ {saborP1}')
+      print(traço)
+elif saborPizza == 2:
+      print(traço)
+      print(f'Valor atual: R$ {saborP2}')
+      print(traço)
+elif saborPizza == 3:
+      print(traço)
+      print(f'Valor atual: R$ {saborP3}')
+      print(traço)
+elif saborPizza == 4:
+      print(traço)
+      print(f'Valor atual: R$ {saborP4}')
+      print(traço)
+elif saborPizza == 5:
+      print(traço)
+      print(f'Valor atual: R$ {saborP5}')
+      print(traço)
+else:
+      print(traço)
+      print(f'Tente novamente, digite numeros de 1 a 4.')
+      print(traço)
+print('--->Bem Vindo a Pizzaria RPM<---')
+print(traço)
+# Tamanho da Pizza
+tamanhoP = int(input('1 - Pequena (+ R$ 15,00'))
+print('-->Agora, qual seria o tamanho da Pizza?<--')
+if tamP1 == 0:
+      print(traço)
+      print('Ok, até logo!!!')
+      print(traço)
+elif saborPizza == 1:
+      print(traço)
+      print(f'Valor atual está: R$ {saborP1}')
+      print(traço)
+elif saborPizza == 2:
+      print(traço)
+      print(f'Valor atual: R$ {saborP2}')
+      print(traço)
+elif saborPizza == 3:
+      print(traço)
+      print(f'Valor atual: R$ {saborP3}')
+      print(traço)
+elif saborPizza == 4:
+      print(traço)
+      print(f'Valor atual: R$ {saborP4}')
+      print(traço)
